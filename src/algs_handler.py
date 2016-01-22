c1e6635d72b0e43f3311dc89a5fe85d677313f7a
Andres Ferraro <andres.ferraro@upf.edu>
2016-01-22 18:33:14 +0100

    Adding webserver

---
+++ src/algs_handler.py
@@ -0,0 +1,11 @@
+
+
+class AlgsHandler():
+    def get_recommend_algs(self):
+        pass
+    def recommend(self, objid, system):
+        '''Given an system and a id of recording or artist,
+        computes de recommendation following the system specification.
+        
+        '''
+        pass
